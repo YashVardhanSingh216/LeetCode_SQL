f14846b30f32c5ae3af50b01b609d2f02419a358
YashVardhanSingh216 <yashvardhansingh216@gmail.com>
2026-09-12 13:35:44 +0530

    Create 176. Second Highest Salary.sql

---
+++ 176. Second Highest Salary.sql
@@ -0,0 +1,6 @@
+-- 176. Second Highest Salary
+
+# Write your MySQL query statement below
+
+SELECT MAX(salary) AS 'SecondHighestSalary' FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee)
